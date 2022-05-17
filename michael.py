8fd63e0d7b5d93f7d0360b7ea09366d459326e1f
ka7eep <90337036+ka7eep@users.noreply.github.com>
2022-05-17 12:01:45 -0600

    minh fix the spacing

--- michael.py
+++ michael.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
     print('Michael was here')
-    print ('Hey everybody Dan was here')
+    print('Hey everybody Dan was here')
     print('Jeremy was here')
     print('Minh CT was here')
